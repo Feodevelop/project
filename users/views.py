6bd7c9f9c174baf09ac5197a925b15f03e9b7290
Feodevelop <leejh52972@gmail.com>
2024-08-30 13:40:32 +0900

    일부 수정.

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def login(request):
     return render(request, 'login.html')
@@ -8,3 +12,22 @@ def signup(request):
 
 def mypage(request):
     return render(request, 'mypage.html')
+
+# 로그인 화면 
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/main")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form":form})
